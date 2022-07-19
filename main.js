cc3e71f91dc8f8ce1715bc974201efa11420d5ce
Harshul Rai <harshuljps@gmail.com>
2022-07-20 00:01:21 +0530

    Task 11

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-function store() {
-    var username = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+windows.onload = function() {
+    function store(event) {
+        event.preventDefault();
+        const username = event.target.name.value;
+        const email = event.target.email.value;
+        
+        // var name = localStorage.setItem("username", username);
+        // var emailID = localStorage.setItem("email", email);
     
-    var name = localStorage.setItem("username", username);
-    var emailID = localStorage.setItem("email", email);
-
-    const obj = {
-        username: username,
-        email: email
-
+        const obj = {
+            username: username,
+            email: email
+    
+        }
+    
+        
+        // localStorage.setItem('userDetails', obj_string);
+    
+         localStorage.setItem(obj.email, JSON.stringify(obj));
+         userOnScreen(obj)   
+    
     }
 
-    let obj_string = JSON.stringify(obj);
-    localStorage.setItem('userDetails', obj_string);
-
-    console.log(JSON.parse(obj_string));
-
+    function userOnScreen(user) {
+        const parentNode = document.getElementById('listofusers');
+        const childHTML = `<li> ${user.username} -${user.email} </li>`
+    
+        parentNode.innerHTML = childHTML;
+    }
 }
+
+
+function userOnScreen(user) {
+    const parentNode = document.getElementById('listofusers');
+    const childHTML = `<li> ${user.username} -${user.email} </li>`
+
+    parentNode.innerHTML = childHTML;
+}
+
+window.onbeforeunload = function() {
+    localStorage.setItem('username', $('#name').val());
+    localStorage.setItem('email', $('#email').val());
+}
+
 
 
 
