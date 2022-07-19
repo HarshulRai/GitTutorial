63bd473edf1240912d993d767509923cb401e9fd
Harshul Rai <harshuljps@gmail.com>
2022-07-19 22:53:48 +0530

    task 11

--- main.js
+++ main.js
@@ -5,8 +5,19 @@ function store() {
     var name = localStorage.setItem("username", username);
     var emailID = localStorage.setItem("email", email);
 
+    const obj = {
+        username: username,
+        email: email
+
+    }
+
+    let obj_string = JSON.stringify(obj);
+    localStorage.setItem('userDetails', obj_string);
+
+    console.log(JSON.parse(obj_string));
 
 }
+
 
 
 
